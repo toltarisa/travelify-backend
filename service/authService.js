ea69ae9121bb19cf3c3f3144c63b34b08ed53be4
Isa Toltar <isatoltar@gmail.com>
2021-01-18 23:35:45 +0300

    mailer transport configured

--- service/authService.js
+++ service/authService.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  ignoreTLS: false,
   auth: {
     user: process.env.NODEMAILER_TRANSPORTER_USER,
     pass: process.env.NODEMAILER_TRANSPORTER_PASS,
